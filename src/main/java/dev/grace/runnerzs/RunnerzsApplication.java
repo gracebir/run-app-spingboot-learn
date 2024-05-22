03d6a82d36baed963da2c5236be200dd6ac96aef
gracebir <grace.devolop@gmail.com>
2024-05-22 20:48:16 +0200

    implement a runer

--- src/main/java/dev/grace/runnerzs/RunnerzsApplication.java
+++ src/main/java/dev/grace/runnerzs/RunnerzsApplication.java
@@ -1,9 +1,19 @@
 package dev.grace.runnerzs;
 
+import dev.grace.runnerzs.user.User;
+import dev.grace.runnerzs.user.UserHttpClient;
+import dev.grace.runnerzs.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @SpringBootApplication
 public class RunnerzsApplication {
@@ -12,6 +22,24 @@ public class RunnerzsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzsApplication.class, args);
 		logger.info("Application started succeessfully");
+	}
+
+	@Bean
+	UserHttpClient userHttpClient(){
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client){
+		return args -> {
+			List<User> users = client.findAll();
+			System.out.println(users);
+
+			User user = client.findById(1);
+			System.out.println(user);
+		};
 	}
 
 }
